7dd5395d65103de573345c9fde96491511f3b471
mrLuisFer <mrluisfeer@gmail.com>
2021-08-19 19:58:37 -0500

    fix: separating the handle of change event

--- src/js/main.js
+++ src/js/main.js
@@ -1,28 +1,12 @@
 import "../css/style.css";
 import { Octokit } from "https://cdn.skypack.dev/@octokit/core";
-import { profileCard } from './profileCard.js'
+import { handleChangeEvent } from "./handleChangeEvent";
 
 const ghToken = import.meta.env.VITE_GH_TOKEN;
 const octokit = new Octokit({ auth: ghToken });
 
-// const response = await octokit.request("GET /users/{username}", {
-//   username: "mrLuisFer",
-// });
-
 const inputUsername = document.getElementById("gh-username");
-const content = document.getElementById("content");
 
-let user;
-inputUsername.addEventListener("change", async (event) => {
-  const username = event.target.value;
-  const response = await octokit.request("GET /users/{username}", {
-    username: username,
-  });
-  console.log(response);
-
-  if (response.status === 200) {
-    user = response.data;
-
-    content.innerHTML = profileCard(user)
-  }
+inputUsername.addEventListener("change", (event) => {
+  handleChangeEvent(event, octokit);
 });

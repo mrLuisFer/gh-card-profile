0968d537c312e2337b1b692d8de978aead7b0d22
mrLuisFer <mrluisfeer@gmail.com>
2021-08-21 09:57:09 -0500

    fix: removing vite client token

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,7 @@ import '../css/attribution.css'
 import { Octokit } from 'https://cdn.skypack.dev/@octokit/core'
 import { handleChangeEvent } from './handleChangeEvent'
 
-const ghToken = import.meta.env.VITE_GH_TOKEN
+const ghToken = import.meta.env.GH_TOKEN
 const octokit = new Octokit({ auth: ghToken })
 
 const inputUsername = document.getElementById('gh-username')
